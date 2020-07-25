8df21ee9d8c5ae6b461a6394e64c9f2bfe4694da
kas2020-commits <kas2020@protonmail.com>
2020-07-25 18:24:43 -0400

    minor tweaks

--- user.sh
+++ user.sh
@@ -9,28 +9,32 @@ if [ ${what_distro} -eq 1 ]; then
 	fontawesome="fontawesome-fonts"
 	firacode="fira-code-fonts"
 	noto_fonts="google-noto-fonts-common"
-	xinit="xorg-x11-xinit"
 	sudo dnf copr enable opuk/pamixer
+	xorg="xorg-x11-xinit xorg-x11-server-common xorg-x11-server-devel xorg-x11-server-Xorg xorg-x11-server-utils"
+	xorg_devel="libXinerama-devel libXft-devel libX11-devel libXext-devel xorg-x11-server-devel"
 elif [ ${what_distro} -eq 2 ]; then
 	instal="pacman -S"
 	jetbrains="ttf-jetbrains-mono"
 	fontawesome="otf-font-awesome"
 	firacode="ttf-fira-code"
 	noto_fonts="noto-fonts"
-	xinit="xorg-xinit"
+	xorg="xorg-xinit xorg-server xorg-xsetroot"
+	xorg_devel=""
 fi
 
-# Important Utilities
-sudo ${install} openssh dash xdg-user-dirs pulseaudio pamixer zsh playerctl
+# utils
+sudo ${install} openssh dash xdg-user-dirs pulseaudio pamixer zsh playerctl flameshot
 
 # fonts
 sudo ${install} ${jetbrains} ${fontawesome} ${firacode} ${noto_fonts}
 
-# Desktop
-sudo ${install} xorg-server ${xinit} xorg-xsetroot feh xlcip xsel \
-	imagemagick picom lxappearance-gtk3 flameshot sxhkd i3lock
+# Xorg
+sudo ${install} ${xorg} ${xorg_devel}
 
-# Neovim/vim-plug
+# Xorg utils
+sudo ${install} feh imagemagick i3lock picom xsel xclip sxhkd
+
+# Neovim plugin manager
 curl -fLo "$HOME"/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -38,20 +42,20 @@ curl -fLo "$HOME"/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 sudo ${install} zathura zathura-pdf-poppler mpv firefox nnn cmus rofi neovim sxiv
 sudo ln -s /usr/bin/rofi /usr/bin/dmenu
 
-# Defaults
+# Default applications
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 
-## moving files to the right spots
+## directory setup
 if [ ${what_distro} -eq 2 ]; then
 	sudo /usr/bin/ln -sfT dash /usr/bin/sh
 	sudo mv dash.hook /usr/share/libalpm/hooks/bash-update.hook
 fi
 sudo mkdir -p /etc/X11/xorg.conf.d
 sudo mv mouse_accel.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-xdg-user-dirs-update
-sudo usermod -s /bin/zsh "$USER"
-
-## Create important dirs
 mkdir -p "$HOME"/Pictures/Screenshots
 mkdir -p "$HOME"/.local/github
 mkdir -p "$HOME"/.cache/zsh
+
+## refresh & configure utils
+xdg-user-dirs-update
+sudo usermod -s /bin/zsh "$USER"
