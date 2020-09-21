3f85fad7624a75f41f73007eaae44c51ad57fbe3
kas2020-commits <kas2020@protonmail.com>
2020-09-20 23:25:51 -0400

    new_init

--- git_setup.sh
+++ git_setup.sh
@@ -1,19 +1,20 @@
+#!/bin/sh
+
+[ $# -ne 2 ] && echo \
+	"You must supply your git username and email (space separated in that order)" \
+	&& exit 1
+
 ## Git
-echo "input the email for git/ssh:"
-read email
-echo "input git username:"
-read username
-git config --global user.name "$username"
-git config --global user.email "$email"
-ssh-keygen -t rsa -b 4096 -C "$email"
+git config --global user.name $1
+git config --global user.email $2
+
+ssh-keygen -t rsa -b 4096 -C $2
 ssh-add "$HOME"/.ssh/id_rsa
+
 echo "add the following public key to your github then press enter:"
 cat ~/.ssh/id_rsa.pub
-echo ""
+# A stopper to allow for the user to do stuff
 read bull
 
 git_projects="$HOME"/.local/github
-git clone git@github.com:kas2020-commits/dwm.git "$git_projects"/dwm
-git clone git@github.com:kas2020-commits/st.git "$git_projects"/st
-git clone git@github.com:kas2020-commits/archstall.git "$git_projects"/archinstall
-git clone git@github.com:kas2020-commits/dootfiles.git "$git_projects"/dootfiles
+mkdir -p ${git_projects}
--- root.sh
+++ root.sh
@@ -1,51 +1,40 @@
 #!/bin/sh
 
-printf "Chose 1 of the supported distros:\n1) Fedora (rpm)\n2) Arch (pacman)\n"
-read what_distro
-
-if [ ${what_distro} -eq 1 ]; then
-	instal="dnf install"
-	jetbrains="jetbrains-mono-fonts"
-	fontawesome="fontawesome-fonts"
-	firacode="fira-code-fonts"
-	noto_fonts="google-noto-fonts-common"
-	xorg="xorg-x11-xinit xorg-x11-server-common xorg-x11-server-devel xorg-x11-server-Xorg xorg-x11-server-utils"
-	xorg_devel="libXinerama-devel libXft-devel libX11-devel libXext-devel xorg-x11-server-devel"
-	sudo dnf copr enable opuk/pamixer
-	dnf copr enable skidnik/clipmenu
-elif [ ${what_distro} -eq 2 ]; then
-	instal="pacman -S"
-	jetbrains="ttf-jetbrains-mono"
-	fontawesome="otf-font-awesome"
-	firacode="ttf-fira-code"
-	noto_fonts="noto-fonts"
-	xorg="xorg-xinit xorg-server xorg-xsetroot"
-	xorg_devel=""
-fi
+install_cmd="pacman -S --noconfirm"
 
 # utils
-${install} openssh dash xdg-user-dirs pulseaudio pamixer zsh playerctl flameshot
+${install_cmd} openssh dash xdg-user-dirs pulseaudio pamixer zsh playerctl flameshot
+
+# C/C++ development
+${install_cmd} clang gcc
+
+# Computational development
+${install_cmd} octave python-pip
 
 # GNU/Stow is the util used for the dotfiles repo
-${install} stow
+${install_cmd} stow
 
 # fonts
-${install} ${jetbrains} ${fontawesome} ${firacode} ${noto_fonts}
+${install_cmd} ttf-jetbrains-mono otf-font-awesome ttf-fira-code noto-fonts \
+	ttf-inconsolata
 
 # Xorg
-${install} ${xorg} ${xorg_devel}
+${install_cmd} xorg-xinit xorg-server xorg-xsetroot
 
-# Xorg utils
-${install} feh imagemagick i3lock picom xsel xclip sxhkd
+# Editor + Common Tools
+${install_cmd} neovim xsel xclip zathura zathura-pdf-poppler
+
+# Window-Manager specific installs
+${install_cmd} i3lock picom sxhkd nitrogen imagemagick
 
 # Apps
-${install} zathura zathura-pdf-poppler mpv firefox nnn cmus rofi neovim sxiv
+${install_cmd} mpv firefox nnn cmus rofi sxiv
 ln -s /usr/bin/rofi /usr/bin/dmenu
 
-## directory setup
+## Changing default shell to dash
 [ -f /usr/bin/dash ] && /usr/bin/ln -sfT dash /usr/bin/sh
-if [ ${what_distro} -eq 2 ]; then
-	mv dash.hook /usr/share/libalpm/hooks/bash-update.hook
-fi
-mkdir -p /etc/X11/xorg.conf.d
-mv mouse_accel.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+cp dash.hook /usr/share/libalpm/hooks/bash-update.hook
+
+# fixing the mouse acceleration for WM's. If you use a DE, this is not
+# recommended
+[ -d /etc/X11/xorg.conf.d ] && cp mouse_accel.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
